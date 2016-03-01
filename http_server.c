1f5209444bdda5645ab79077ec6eb736ac9729bd
Conor McGrory <cpmcgrory@gmail.com>
2016-03-01 07:47:01 +0000

    Refactored code into a few function blocks, and started working on parsing HTTP requests

--- http_server.c
+++ http_server.c
@@ -42,7 +42,7 @@ void sigchld_handler(int s)
 }
 
 
-// get sockaddr, IPv4 or IPv6:
+/* get sockaddr, IPv4 or IPv6: */
 void *get_in_addr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET) {
@@ -51,210 +51,253 @@ void *get_in_addr(struct sockaddr *sa)
 
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
+/* Create the socket the server is going to listen on, and bind it
+ * to the given port. If this fails, throw an error and return -1. 
+ * This code is almost completely from Beej's Guide to Network 
+ * Programming.
+ */
+int create_listen_socket(char* port) 
+{
+  const int yes = 1;
+
+  int sockfd;
+  int rv;
+  
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr;
+  
+  /* Set values in hints */
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  /* Use hints to set values in servinfo */
+  if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return -1;
+  }
+
+  /* Loop through all the results and bind to the first we can */
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			 p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+		   sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+    
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+    
+    break;
+  }
+    
+  freeaddrinfo(servinfo); // all done with this structure
+  
+  if (p == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    return -1;
+  }
+  
+  /* If we reach this point without returning -1, then sockfd 
+   * is the file descriptor of a valid socket bound to the 
+   * given port */
+  return sockfd;
+}
+
+
+int process_request(new_fd) 
+{
+  
+  char buf[BUFMAX]; // Buffer to store incoming data 
+
+  int buflen; // Number of bytes loaded in to buffer
+
+  struct ParsedRequest *req;
+  //struct ParsedHeader *header;
+
+  /* Receive data from client and read it in to buffer */
+  if ((buflen = recv(new_fd, buf, BUFMAX - 1, 0)) == -1) {
+    perror("receiving error");
+    return(-1);
+  }
+  
+  printf("server: received: '%s'\n", buf);
+  
+  req = ParsedRequest_create();
+  
+  // try to parse and send badreqmsg if bad request
+  if (ParsedRequest_parse_server(req, buf, buflen) != 0) {
+    // send(new_fd, badReqMsg, strlen(badReqMsg), 0);
+    perror("parse failed");
+    return -1;
+  }
+
+  /* If parse is successful, print out data */
+  printf("Method:%s\n", req->method);
+  printf("Protocol:%s\n", req->protocol);
+  printf("Host:%s\n", req->host);
+  printf("Path:%s\n", req->path);
+  printf("Version:%s\n", req->version);
+
+  if (strcmp(req->version, "HTTP/1.0") != 0) {
+    perror("wrong version");
+    ParsedRequest_destroy(req);
+    return -1;
+  }
+  
+  // READ FILE
+  FILE *file;
+  char *buffer;
+  int fileLength;
+  
+  // Open file
+  file = fopen(req->path + 1, "rb"); // remove open slash
+  if (!file) {
+    perror("cant find file");
+    return -1;
+  }
+  
+  // Get file length
+  fseek(file, 0, SEEK_END);
+  fileLength=ftell(file);
+  fseek(file, 0, SEEK_SET);
+  
+  // Allocate memory
+  buffer=(char *)malloc(fileLength+1);
+  if (!buffer){
+    perror("Memory error");
+    fclose(file);
+    return -1;
+  }
+  
+  // store file in buffer
+  fread(buffer, fileLength, 1, file);
+  fclose(file);
+  
+  // TODO: need to figure out what file type
+  char header[1000];
+  char * file_type;
+  char * status_line = "figure out how to get status";
+  
+  sprintf(header, 
+	  "%s\r\n"
+	  "Connection: close\r\n"
+	  "Content-Length: %i\r\n"
+	  "Content-Type: %s\r\n"
+	  "\r\n", status_line, fileLength, file_type);
+  
+  int responseLength = fileLength + strlen(header);
+  
+  // create response from header and file
+  char * response = (char*)malloc(responseLength);
+  strcpy(response, header);
+  memcpy(response+strlen(header), buffer, fileLength);
+  
+  // send header and file through HTTP
+  send(new_fd, response, responseLength, 0);
+  
+  printf("here");
+  
+  /* Call destroy on any ParsedRequests that you create 
+   * once you are done using them. This will free memory 
+   * dynamically allocated by the proxy_parse library. */
+  ParsedRequest_destroy(req);
+  
+  /* Return success status */
+  return 0;
+} 
+ 
 	
 int main(int argc, char * argv[])
 {
 
-    char* port = argv[1];
+  char* port = argv[1]; // Port to listen on
+  char s[INET6_ADDRSTRLEN];
 
-    char buf[BUFMAX];   // use this to read stuff
+  int sockfd; // socket to listen on
+  int new_fd;  // socket for new connections
 
-    struct ParsedRequest *req;
-    struct ParsedHeader *header;
-    FILE *filep;
-    long fsize;
-    char *version;
-    int numbytes;
+  struct sigaction sa;
 
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector"s address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
+  /* Connector's address information */
+  struct sockaddr_storage their_addr; 
+  socklen_t sin_size = sizeof their_addr;
 
-    char s[INET6_ADDRSTRLEN];
-    int rv;
+  /* Create socket on specified port to listen on */
+  sockfd = create_listen_socket(port);
+  
+  /* Listen for incoming connections */
+  if (listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
 
-    int length;
+  /* Reap all dead processes */
+  sa.sa_handler = sigchld_handler; 
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+  
+  printf("server: waiting for connections...\n");
 
-    char response[BUFMAX];   // holds our response
+  /* Main accept() loop */  
+  while(1) {  
+    
+    /* Accept new connection */
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+    
+    inet_ntop(their_addr.ss_family,
+	      get_in_addr((struct sockaddr *)&their_addr),
+	      s, sizeof s);
 
-    // from beej
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    printf("server: got connection from %s\n", s);
+    fflush(stdout); // (just making sure!)
 
-    // from beej
-    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+    /* Child process */
+    if (fork() == 0) { 
+
+      /* Close listener socket -- child doesn't need this */
+      close(sockfd);
+      
+      /* Call process_request method */
+      if (process_request(new_fd) == -1) {
+	perror("Error processing request");	
+	close(new_fd);
+	exit(EXIT_FAILURE);
+      }
+
+      /* Close new_fd and exit with success status */
+      close(new_fd);
+      exit(EXIT_SUCCESS);
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
-
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-
-        break;
+    /* Parent process */
+    else { 
+      /* Close new_fd and return to beginning of loop */
+      close(new_fd); 
     }
-
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        exit(1);
-    }
-
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
-
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-
-    printf("server: waiting for connections...\n");
-
-    while(1) {  // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("server: got connection from %s\n", s);
-
-        if (!fork()) { // this is the child process
-            // close(sockfd); // child doesn"t need the listener
-            if ((numbytes = recv(new_fd, buf, BUFMAX-1, 0)) == -1) {
-                 perror("receiving error");
-                 exit(1);
-               }
-
-            printf("server: received: '%s'\n", buf);
-
-
-            printf("Method:%s\n", req->method);
-            printf("Protocol:%s\n", req->protocol);
-            printf("Host:%s\n", req->host);
-            printf("Path:%s\n", req->path);
-            printf("Version:%s\n", req->version);
-
-            req = ParsedRequest_create();
-
-            // try to parse and send badreqmsg if bad request
-            if (ParsedRequest_parse(req, buf, length) != 0) {
-                // send(new_fd, badReqMsg, strlen(badReqMsg), 0);
-                perror("parse failed");
-                return -1;
-            }
-            if (strcmp(req->version, "HTTP/1.0") != 0) {
-                perror("wrong version");
-                ParsedRequest_destroy(req);
-                close(new_fd);
-                exit(1);
-            }
-
-            // READ FILE
-            FILE *file;
-            char *buffer;
-            int fileLength;
-
-            //Open file
-            file = fopen(req->path + 1, "rb"); // remove open slash
-            if (!file)
-            {
-                printf("cant find file");
-                exit(1);
-            }
-
-            //Get file length
-            fseek(file, 0, SEEK_END);
-            fileLength=ftell(file);
-            fseek(file, 0, SEEK_SET);
-
-            //Allocate memory
-            buffer=(char *)malloc(fileLength+1);
-            if (!buffer)
-            {
-                fprintf(stderr, "Memory error");
-                fclose(file);
-                exit(1);
-            }
-
-            // store file in buffer
-            fread(buffer, fileLength, 1, file);
-            fclose(file);
-
-            // TODO: need to figure out what file type
-            char header[1000];
-            char * file_type;
-            char * status_line = "figure out how to get status";
-
-            sprintf(header, 
-            "%s\r\n"
-            "Connection: close\r\n"
-            "Content-Length: %i\r\n"
-            "Content-Type: %s\r\n"
-            "\r\n", status_line, fileLength, file_type);
-
-            int responseLength = fileLength + strlen(header);
-
-            // create response from header and file
-            char * response = (char*)malloc(responseLength);
-            strcpy(response, header);
-            memcpy(response+strlen(header), buffer, fileLength);
-
-            // send header and file through HTTP
-            send(new_fd, response, responseLength, 0);
-
-            printf("here");
-
-            // Call destroy on any ParsedRequests that you
-            // create once you are done using them. This will
-            // free memory dynamically allocated by the proxy_parse library. 
-            ParsedRequest_destroy(req);
-            close(new_fd);
-            exit(0);
-
-            // close(sockdf);
-
-            // if (send(new_fd, "Hello, world!", 13, 0) == -1)
-            //     perror("send");
-            // close(new_fd);
-            // exit(0);
-        }
-        close(new_fd);  // parent doesn"t need this
-    }
-
-	return 0;
+  }
+  
+  return 0;
 }
 
 
