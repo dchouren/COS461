ef304bc224d24518b37a020683769defdb4cc5e9
Daway Chou-Ren <dchouren@princeton.edu>
2016-03-03 02:42:24 -0500

    started proxy

--- proxy.go
+++ proxy.go
@@ -3,6 +3,9 @@ package main
 import (
     "fmt"
     "os"
+    "net"
+    "net/http"
+    "bufio"
 )
 
 const EXIT_FAILURE = 1
@@ -12,8 +15,48 @@ func usage() {
     os.Exit(EXIT_FAILURE)
 }
 
+func handleConnection(connection net.Conn) {
+    defer connection.Close()
+
+    request, error := http.ReadRequest(bufio.NewReader(connection))
+    url := request.URL
+
+    request_line := []byte("GET " + url + " HTTP/1.0\r\n")
+
+}
+
 func main() {
     if len(os.Args) != 2 {
         usage()
     }
+    port = ":" + os.Args[1]
+
+    tcp_address, err := net.ResolveTCPAddr("tcp", port)
+
+    ln, err := net.Listen("tcp", port)
+    if err != nil {
+        // handle error
+    }
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            // handle error
+        }
+        go handleConnection(conn)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
