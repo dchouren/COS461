895171cf5a07d9bea605b312dc64708238385ed2
Daway Chou-Ren <dchouren@princeton.edu>
2016-03-03 22:22:04 -0500

    proxy working

--- proxy.go
+++ proxy.go
@@ -6,6 +6,9 @@ import (
     "net"
     "net/http"
     "bufio"
+    "io"
+    "bytes"
+    "strings"
 )
 
 const EXIT_FAILURE = 1
@@ -15,13 +18,55 @@ func usage() {
     os.Exit(EXIT_FAILURE)
 }
 
-func handleConnection(connection net.Conn) {
-    defer connection.Close()
 
-    request, error := http.ReadRequest(bufio.NewReader(connection))
+func handleConnection(old_conn net.Conn) {
+
+    println("here")
+
+    defer old_conn.Close()
+
+    // read request
+    request, error := http.ReadRequest(bufio.NewReader(old_conn))
+    if error != nil {
+        // TODO: send 400 error
+        os.Exit(1)
+    }
+    if request.Method != "GET" {
+        // TODO: send 501 error
+        os.Exit(1)
+    }
     url := request.URL
+    path := url.Path
+    host := request.URL.Host
 
-    request_line := []byte("GET " + url + " HTTP/1.0\r\n")
+    if (!strings.Contains(host, ":")) {
+        host += ":80"
+    }
+
+    // open connection
+    new_conn, error := net.Dial("tcp", host)
+    if error != nil {
+        // handle error
+        os.Exit(1)
+    }
+    defer new_conn.Close()
+
+    //send new request
+    // var get_buffer bytes.Buffer
+    // get_buffer.WriteString("GET ")
+    // get_buffer.WriteString(path)
+    // get_buffer.WriteString(" HTTP/1.0\r\n")
+    new_conn.Write([]byte("GET " + path + " HTTP/1.0\r\n"))
+    request.Header.Add("Connection", "close")
+    request.Header.Add("Host", host)
+
+    request.Header.Write(new_conn)
+    new_conn.Write([]byte("\r\n\r\n"))
+
+    // read response
+    var buf bytes.Buffer
+    io.Copy(&buf, new_conn)
+    buf.WriteTo(old_conn)
 
 }
 
@@ -29,11 +74,14 @@ func main() {
     if len(os.Args) != 2 {
         usage()
     }
-    port = ":" + os.Args[1]
+    var port string = ":" + os.Args[1]
 
     tcp_address, err := net.ResolveTCPAddr("tcp", port)
+    if err != nil {
+        // handle error
+    }
 
-    ln, err := net.Listen("tcp", port)
+    ln, err := net.ListenTCP("tcp", tcp_address)
     if err != nil {
         // handle error
     }
@@ -45,6 +93,7 @@ func main() {
         go handleConnection(conn)
     }
 }
+
 
 
 
